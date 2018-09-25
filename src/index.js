fcd587139cd4b70e97826b214d74be7009b408e9
Olga Luk <olga_ivanova_86@mail.ru>
2018-09-25 12:23:35 +0300

    function of least complexity

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+  let length = 0;
+  let result = 0;
+  let prepareArr = {};
+  let len = array.length;
+  for(let i = 0; i<len; i++) {
+    prepareArr[array[i]] = array[i] +1;
+  }
+  for(let number in prepareArr) {
+    let nextElem = prepareArr[number];
+    while (prepareArr[nextElem] !== undefined) {
+      prepareArr[number] = prepareArr[nextElem];
+      delete prepareArr[nextElem];
+      nextElem = prepareArr[number];
+    }
+    length = nextElem - number;
+    if (length > result) {
+      result = length;
+    }
+  }
+    return result;
+  }
+
